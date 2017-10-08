d86ae235eafd0ce325fb5fd3280d78a1b26303d7
zz_zigzag <zz_zigzag@outlook.com>
2017-10-08 22:43:51 +0800

    update pom.xml for package(jar runable)

--- src/test/java/site/xiaodong/wechat/BillTest.java
+++ src/test/java/site/xiaodong/wechat/BillTest.java
@@ -10,11 +10,11 @@ public class BillTest {
 		Bill.main(args);
 	}
 
-	@Test
-	public void testDeleted() {
-		String args[] = { "-f", "EnMicroMsg.db", "-c", "没坏账饭友群", "-s", "2017-09-01", "-e", "2017-10-01", "-d" };
-		Bill.main(args);
-	}
+//	@Test
+//	public void testDeleted() {
+//		String args[] = { "-f", "EnMicroMsg.db", "-c", "没坏账饭友群", "-s", "2017-09-01", "-e", "2017-10-01", "-d" };
+//		Bill.main(args);
+//	}
 
 	@Test
 	public void testDetail() {
